52ba05aa0d071460b4d53678410f238466494f91
Ben Baer <benbaer17@gmail.com>
2018-12-03 22:39:58 -0500

    Not quite finished with HiFi, but close...

--- web/index.php
+++ web/index.php
@@ -24,6 +24,64 @@
     } else {
       $dbOk = true; 
     }
+    
+    // Now let's process our form:
+  // Have we posted?
+  $havePostTask = isset($_POST["save_task"]);
+  $havePostTrade = isset($_POST["save_trade"]);
+  $havePostMember = isset($_POST["save_member"]);
+  
+  if($havePostTask){
+    $jobdesc = htmlspecialchars(trim($_POST["jobdesc"]));
+    $priority = htmlspecialchars(trim($_POST["priority"]));
+    $time = htmlspecialchars(trim($_POST["time"]));
+    $status = htmlspecialchars(trim($_POST["status"])); 
+  }
+  if($havePostTrade){
+    $traderid = htmlspecialchars(trim($_POST["traderid"]));
+    $tradeeid = htmlspecialchars(trim($_POST["tradeeid"]));
+    $jobid = htmlspecialchars(trim($_POST["jobid"]));
+  }
+  if($havePostMember){
+    $firstname = htmlspecialchars(trim($_POST["firstname"]));
+    $lastname = htmlspecialchars(trim($_POST["lastname"]));
+  }
+
+  if($dbOk){
+    if($havePostTask){
+      $jobdescForDb =trim($_POST["jobdesc"]);
+      $priorityForDb = trim($_POST["priority"]);
+      $timeForDb = trim($_POST["time"]);
+      $statusForDb = trim($_POST["status"]);
+      $insQuery = "insert into allTasks (`jobdesc`, `priority`, `time`, `status`) values(?,?,?,?)";
+      $statement = $db->prepare($insQuery);
+      $statement->bind_param("siis",$jobdescForDb,$priorityForDb,$timeForDb,$statusForDb);
+      $statement->execute();
+      $statement->close();
+      header("Refresh:0");
+    }
+    if($havePostTrade){
+      $traderidForDb=trim($_POST["traderid"]);
+      $tradeeidForDb= trim($_POST["tradeeid"]);
+      $jobidForDb= trim($_POST["jobid"]);
+      $insQuery = "insert into trades (`traderid`, `tradeeid`, `jobid`) values(?,?,?)";
+      $statement = $db->prepare($insQuery);
+      $statement->bind_param("iii", $traderidForDb, $tradeeidForDb, $jobidForDb);
+      $statement->execute();
+      $statement->close();
+      header("Refresh:0");
+    }
+    if($havePostTask){
+      $firstnameForDb=trim($_POST["firstname"]);
+      $lastnameForDb= trim($_POST["lastname"]);
+      $insQuery = "insert into teamMembers (`firstname`, `lastname`) values(?,?)";
+      $statement = $db->prepare($insQuery);
+      $statement->bind_param("ss", $firstnameForDb, $lastnameForDb);
+      $statement->execute();
+      $statement->close();
+      header("Refresh:0");
+    }
+  }
   ?>
 
   <body>
@@ -48,7 +106,7 @@
               $query = 'select * from allTasks order by jobid';
               $result = $db->query($query);
               $numRecords = $result->num_rows;
-              echo '<ul>';
+              echo '<ul id="allChoresList">';
               for ($i=0; $i < $numRecords; $i++) {
                 $record = $result->fetch_assoc();
                 echo '<li><button id="job-';
@@ -57,7 +115,7 @@
                 echo htmlspecialchars($record['jobdesc']);
                 echo '</button></li>';
               }
-              echo '<li><button id="add" class="barbutton">+ Add more chores</button></li>';
+              echo '<li><button id="add-task" class="barbutton" onclick="addTask()">+ Add more chores</button></li>';
               echo '</ul>';
               $result->free();
             }
@@ -70,7 +128,7 @@
               $query = 'select * from myTasks order by jobid';
               $result = $db->query($query);
               $numRecords = $result->num_rows;
-              echo '<ul>';
+              echo '<ul id="myChoresList">';
               for ($i=0; $i < $numRecords; $i++) {
                 $record = $result->fetch_assoc();
                 echo '<li><button id="myjob-';
@@ -79,7 +137,7 @@
                 echo htmlspecialchars($record['jobdesc']);
                 echo '</button></li>';
               }
-              echo '<li><button id="add" class="barbutton">+ Add more chores</button></li>';
+              echo '<li><button id="add-my-task" class="barbutton" onclick="addTask()">+ Add more chores</button></li>';
               echo '</ul>';
               $result->free();
             }
@@ -92,7 +150,7 @@
               $query = 'select * from trades order by tradeid';
               $result = $db->query($query);
               $numRecords = $result->num_rows;
-              echo '<ul>';
+              echo '<ul id="tradesList">';
               for ($i=0; $i < $numRecords; $i++) {
                 $record = $result->fetch_assoc();
                 echo '<li><button id="trade-';
@@ -101,7 +159,7 @@
                 echo htmlspecialchars($record['jobdesc']);
                 echo '</button></li>';
               }
-              echo '<li><button id="add" class="barbutton">+ Offer a trade</button></li>';
+              echo '<li><button id="add-trade" class="barbutton" onclick="suggestTrade()">+ Offer a trade</button></li>';
               echo '</ul>';
               $result->free();
             }
@@ -114,7 +172,7 @@
               $query = 'select * from teamMembers order by memberid';
               $result = $db->query($query);
               $numRecords = $result->num_rows;
-              echo '<ul>';
+              echo '<ul id="teamMembersList">';
               for ($i=0; $i < $numRecords; $i++) {
                 $record = $result->fetch_assoc();
                 echo '<li><button id="member-';
@@ -123,7 +181,7 @@
                 echo htmlspecialchars($record['firstName']) . " " . htmlspecialchars($record['lastName']);
                 echo '</button></li>';
               }
-              echo '<li><button id="add" class="barbutton">+ Add team members</button></li>';
+              echo '<li><button id="add-member" class="barbutton" onclick="addMember()">+ Add team members</button></li>';
               echo '</ul>';
               $result->free();
               
--- web/js/shiduly.js
+++ web/js/shiduly.js
@@ -2,6 +2,73 @@ function settingsDropdown() {
     document.getElementById("settingsDropdown").classList.toggle("show");
 }
 
+function addTask() {
+  var task_add = document.createElement("div");
+  task_add.style.position = "absolute";
+  task_add.style.width = "33%";
+  task_add.style.height = "90%";
+  task_add.style.top = "90px";
+  task_add.id = "task_adder";
+  task_add.style.background = "#aeaeae";
+  task_add_html = "<p>Input the details about the chore</p>"
+  task_add_html += "<form action=\"/add_task.php\">"
+  task_add_html += "Job Description:</br>"
+  task_add_html += "<input type=\"text\" name=\"jobdesc\"\>"
+  task_add
+  task_add_html += "</form>"
+  task_add.innerHTML = task_add_html;
+  document.getElementById("columns").appendChild(task_add);
+  window.onclick = function(event) {
+    if (!event.target.matches('#task_adder') && !event.target.matches('#add-task') && !event.target.matches('#add-my-task') && !event.target.matches('.addbutton')) {
+      document.getElementById("task_adder").remove();
+    }
+  } 
+}
+
+function suggestTrade() {
+  var suggest_trade = document.createElement("div");
+  var members = document.getElementById("teamMembersList").getElementsByTagName("li");
+  suggest_trade.style.position = "absolute";
+  suggest_trade.style.width = "33%";
+  suggest_trade.style.height = "90%";
+  suggest_trade.style.top = "90px";
+  suggest_trade.id = "trade_suggester";
+  suggest_trade.style.background = "#aeaeae";
+  suggest_trade_html = "<p>Select who you want to trade with</p>"
+  suggest_trade_html += "<ul>";
+  for(i = 0; i < members.length-1; i++){
+    suggest_trade_html += "<li>";
+    suggest_trade_html += members[i].innerHTML
+    suggest_trade_html += "</li>";
+  }
+  suggest_trade_html += "</ul>"
+  suggest_trade.innerHTML = suggest_trade_html;
+  document.getElementById("columns").appendChild(suggest_trade);
+  window.onclick = function(event) {
+    if (!event.target.matches('#trade_suggester') && !event.target.matches('#add-trade') && !event.target.matches('.addbutton') && !event.target.matches('.barbutton')) {
+      document.getElementById("trade_suggester").remove();
+    }
+  }
+}
+
+function addMember() {
+  var member_add = document.createElement("div");
+  member_add.style.position = "absolute";
+  member_add.style.width = "33%";
+  member_add.style.height = "90%";
+  member_add.style.top = "90px";
+  member_add.id = "member_adder";
+  member_add.style.background = "#aeaeae";
+  member_add_html = "<p>Input team members name</p>";
+  member_add.innerHTML = member_add_html;
+  document.getElementById("columns").appendChild(member_add);
+  window.onclick = function(event) {
+    if (!event.target.matches('#member_adder') && !event.target.matches('#add-member') && !event.target.matches('.addbutton')) {
+      document.getElementById("member_adder").remove();
+    }
+  }
+}
+
 window.onclick = function(event) {
   if (!event.target.matches('#settingsButton')) {
 
@@ -14,4 +81,4 @@ window.onclick = function(event) {
       }
     }
   }
-} 
+}
